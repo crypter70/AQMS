81e7096a6242aeae47146d1a5b991abf174e8d7e
deva shofa al fathin <devasa@upi.edu>
2024-05-18 16:35:08 +0700

    Initialize Laravel

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,33 @@
+<nav class="mb-70 navbar navbar-expand-lg">
+        <div class="container">
+        <a class="navbar-brand" href="#">
+                <img src="images/logo.png" height="80" alt="">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Overview</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Air Maps</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Breathe Care</a>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search from here..." aria-label="Search">
+      </form>
+      <div class="nav-icons">
+                    <a href="#" class="nav-icon"><i class="fas fa-bell"></i></a>
+                    <a href="#" class="nav-icon"><i class="fa-solid fa-gear"></i></a>
+                    <a href="#" class="nav-icon"><i class="fa-solid fa-circle-question"></i></a>
+                </div>
+            </div>
+        </div>
+    </nav>
