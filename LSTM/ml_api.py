624154663e27c09558af663db82775ca884a0d3c
crypter70 <yosafatcrypter@gmail.com>
2024-06-07 00:08:43 +0700

    Update ml_api.py

--- LSTM/ml_api.py
+++ LSTM/ml_api.py
@@ -9,6 +9,14 @@ from typing import List
 
 app = FastAPI()
 
+# aktivasi device implementasi model, mps untuk GPU Mac OS, dan cuda untuk GPU Windows OS
+if torch.backends.mps.is_available():
+    device = torch.device("mps")
+elif torch.cuda.is_available():
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
+
 # deklarasi kelas LSTM untuk model LSTM
 class LSTMModel(nn.Module):
     def __init__(self, input_size, hidden_size, num_layers): 
@@ -23,15 +31,10 @@ class LSTMModel(nn.Module):
 
 # deklarasi kelas Data
 class Data(BaseModel):
-    data: List[float]   
+    data: List[float]  
 
-# aktivasi device implementasi model, mps untuk GPU Mac OS, dan cuda untuk GPU Windows OS
-if torch.backends.mps.is_available():
-    device = torch.device("mps")
-elif torch.cuda.is_available():
-    device = torch.device("cuda")
-else:
-    device = torch.device("cpu")
+class PredictionData(BaseModel):
+    predicted_value: List[float]
 
 # deklarasi model dengan parameter dan laod model LSTM pada file lokal
 model = LSTMModel(input_size=1, hidden_size=64, num_layers=2).to(device)
@@ -47,8 +50,14 @@ scaler = StandardScaler()
 def home():
     return {"ml_api_check": "OK"}
 
+# method untuk get data input model
+@app.get("/data")
+async def get_data():
+    example_data = [1, 2, 3, 4, 5] 
+    return {"data": example_data}
+
 # method untuk prediksi
-@app.post("/predict")
+@app.post("/predict", response_model=PredictionData)
 async def predict(aqms_data: Data):
     try:
         data = preprocessing(aqms_data.data)
@@ -64,7 +73,10 @@ async def predict(aqms_data: Data):
             predicted_value = scaler.inverse_transform(predicted_value.reshape(-1,1))  
         
         # return hasil prediksi untuk 1 data atau 1 hari
-        return {"predicted_value": predicted_value[0].item()}
+        # return {"predicted_value": predicted_value[0].item()}
+
+        # return hasil prediksi untuk 7 data atau 7 hari
+        return {"predicted_value": predicted_value[:7]}
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
     
